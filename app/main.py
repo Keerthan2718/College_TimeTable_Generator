74d6daab7c983efa09cc7f046259a4646d993b2c
Keerthan2718 <keerthan2718@gmail.com>
2026-08-11 22:41:43 +0530

    Allow production frontend in CORS

--- app/main.py
+++ app/main.py
@@ -32,7 +32,8 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://college-time-table-generator-l91zzgf4i-keerthan2718s-projects.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
